154a0193bf4874f235607dc74e7ffd297bed2939
smartse0k <smartse0k@naver.com>
2024-08-18 02:17:52 +0900

    [Network] Accept 처리

--- Network/Server.cs
+++ Network/Server.cs
@@ -1,19 +1,39 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using Util;
 
 namespace Network
 {
     public class Server
     {
         Socket _listenSocket;
+        Action<Socket> _onClientConnected; // FIXME : 나중에 인터페이스화 해야함
 
-        public Server(int port)
+        public Server(int port, Action<Socket> onClientConnected)
         {
             IPAddress ipAddress = IPAddress.Any;
             IPEndPoint ipEndPoint = new(ipAddress, port);
             _listenSocket = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             _listenSocket.Bind(ipEndPoint);
             _listenSocket.Listen(5);
+
+            _onClientConnected = onClientConnected;
+
+            Logger.Info($"listening at port {port}...");
+
+            Accept();
+        }
+
+        void Accept()
+        {
+            _listenSocket.AcceptAsync().ContinueWith(OnAccept);
+        }
+
+        void OnAccept(Task<Socket> task)
+        {
+            Socket clientSocket = task.Result;
+            _onClientConnected(clientSocket);
+            Accept();
         }
     }
 }
