d7fa5bfb71bc4a6b27bb331088822a2f116cd7ae
smartse0k <smartse0k@naver.com>
2024-08-18 01:46:48 +0900

    Logger 작성

---
+++ Util/Logger.cs
@@ -0,0 +1,50 @@
+﻿namespace Util
+{
+    public class Logger
+    {
+        public enum LogLevel
+        {
+            ERROR,
+            WARN,
+            INFO
+        }
+
+        private LogLevel _logLevel;
+
+        public Logger(LogLevel logLevel)
+        {
+            _logLevel = logLevel;
+        }
+
+        void Print(LogLevel logLevel, String message)
+        {
+            if (logLevel > _logLevel)
+            {
+                return;
+            }
+
+            TextWriter textWriter = logLevel switch
+            {
+                LogLevel.ERROR => Console.Error,
+                _ => Console.Out,
+            };
+
+            textWriter.WriteLine($"[{logLevel}] [{DateTime.Now}] {message}");
+        }
+
+        public void Error(String message)
+        {
+            Print(LogLevel.ERROR, message);
+        }
+
+        public void Warn(String message)
+        {
+            Print(LogLevel.WARN, message);
+        }
+
+        public void Info(String message)
+        {
+            Print(LogLevel.INFO, message);
+        }
+    }
+}
