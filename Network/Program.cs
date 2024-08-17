e3297b0ff49abe55f045dff49b440d86dbb16614
smartse0k <smartse0k@naver.com>
2024-08-18 02:19:00 +0900

    [Network] Server 클래스 테스트

--- Network/Program.cs
+++ Network/Program.cs
@@ -1,16 +1,31 @@
-﻿namespace Network
+﻿using System.Net.Sockets;
+using System.Text;
+using Util;
+
+namespace Network
 {
     public class Program
     {
 
         static void Main(string[] args)
         {
-            Server server = new Server(8888);
+            Logger.LogLevel = LogLevel.INFO;
+
+            Server server = new Server(8888, OnClientConnected);
+            Logger.Info("server started.");
 
             while (true)
             {
                 ;
             }
+        }
+
+        static void OnClientConnected(Socket clientSocket)
+        {
+            Logger.Info($"client connected. remoteEndPoint: {clientSocket.RemoteEndPoint}");
+
+            byte[] welcome = Encoding.UTF8.GetBytes("CLIENT hello");
+            clientSocket.Send(welcome);
         }
     }
 }
