67197c681cf0ff0354171f8d846ffb39e966f18b
smartse0k <smartse0k@naver.com>
2024-08-18 01:47:30 +0900

    Listen 소켓 작성

---
+++ Network/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Network
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Server server = new Server(8888);
+
+            while (true)
+            {
+                ;
+            }
+        }
+    }
+}
---
+++ Network/Server.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Network
+{
+    public class Server
+    {
+        Socket _listenSocket;
+
+        public Server(int port)
+        {
+            IPAddress ipAddress = IPAddress.Any;
+            IPEndPoint ipEndPoint = new(ipAddress, port);
+            _listenSocket = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _listenSocket.Bind(ipEndPoint);
+            _listenSocket.Listen(5);
+        }
+    }
+}
