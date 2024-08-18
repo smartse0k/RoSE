d8e894404c9c23907c36ea4da8c65f26dd95ee44
smartse0k <smartse0k@naver.com>
2024-08-18 22:38:21 +0900

    [Util] byte array를 hex string으로 변한하는 유틸 함수 추가

--- Util/PrettyConverter.cs
+++ Util/PrettyConverter.cs
@@ -2,5 +2,11 @@
 {
     public class PrettyConverter
     {
+        public static string ToHexString(ArraySegment<byte> segment)
+        {
+            byte[] data = segment.ToArray();
+            string[] hex = Array.ConvertAll(data, x => x.ToString("X2"));
+            return string.Join(" ", hex);
+        }
     }
 }
