7ee031af0e58c28c1bdc64bc91e98e1fb156f87e
smartse0k <smartse0k@naver.com>
2024-08-18 22:44:12 +0900

    [HelloServer] Network에 작성된 코드 이동

--- HelloServer/Program.cs
+++ HelloServer/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using Network;
+using Util;
+
+namespace HelloServer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.LogLevel = LogLevel.INFO;
+
+            Server server = new(8888, CreateSession);
+            Logger.Info("server started.");
+
+            while (true)
+            {
+                ;
+            }
+        }
+
+        static Session CreateSession(Socket s)
+        {
+            Session session = new(s);
+            return session;
+        }
+    }
+}
