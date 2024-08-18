6e4cf89e08d64ed22103ef857b436f5024c3da71
smartse0k <smartse0k@naver.com>
2024-08-19 00:22:33 +0900

    [Network] Client 소켓 연결 구현

--- Network/Client.cs
+++ Network/Client.cs
@@ -1,6 +1,21 @@
-﻿namespace Network
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Network
 {
     public class Client
     {
+        public static void Connect(string destination, int port, Func<Socket, Session> createSession)
+        {
+            IPAddress ipAddress = IPAddress.Parse(destination);
+            IPEndPoint ipEndPoint = new(ipAddress, port);
+
+            Socket socket = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.ConnectAsync(ipEndPoint).ContinueWith((Task task) =>
+            {
+                task.Wait();
+                createSession(socket);
+            });
+        }
     }
 }
