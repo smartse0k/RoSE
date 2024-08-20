2b580a19163544350c2f30b1eb70810822108183
smartse0k <smartse0k@naver.com>
2024-08-20 22:17:13 +0900

    [HelloClient] 프로그램 작성

---
+++ HelloClient/HelloSession.cs
@@ -0,0 +1,31 @@
+﻿using Network;
+using System.Net.Sockets;
+using System.Text;
+using Util;
+
+namespace HelloClient
+{
+    public class HelloSession : Session
+    {
+        public HelloSession(Socket socket) : base(socket)
+        {
+        }
+
+        public override void OnConnected()
+        {
+            Logger.Info($"server connected. remoteEndPoint: {_socket.RemoteEndPoint}");
+        }
+
+        public override void OnDisconnected()
+        {
+            Logger.Info($"server disconnected. remoteEndPoint: {_socket.RemoteEndPoint}");
+        }
+
+        public override int OnReceive(ArraySegment<byte> receiveBuffer)
+        {
+            string incoming = Encoding.UTF8.GetString(receiveBuffer.Array!, receiveBuffer.Offset, receiveBuffer.Count);
+            Logger.Info($"data received. data: {incoming}, size: {receiveBuffer.Count}");
+            return receiveBuffer.Count;
+        }
+    }
+}
--- HelloClient/Program.cs
+++ HelloClient/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Text;
+using Network;
+using Util;
+
+namespace HelloClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.LogLevel = LogLevel.INFO;
+
+            Logger.Info("wait for connect...");
+
+            HelloSession session = (HelloSession)Client.Connect("127.0.0.1", 8888, CreateSession).Result;
+
+            while (true)
+            {
+                Console.Write("input> ");
+
+                string? input = Console.ReadLine()?.Trim();
+                if (input == null || input.Length == 0)
+                {
+                    break;
+                }
+
+                byte[] data = Encoding.UTF8.GetBytes(input);
+                session.Send(data);
+            }
+        }
+
+        static Session CreateSession(Socket s)
+        {
+            Session session = new HelloSession(s);
+            return session;
+        }
+    }
+}
