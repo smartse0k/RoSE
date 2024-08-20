86bb1212cb674465ac5dd71652153066ca870856
smartse0k <smartse0k@naver.com>
2024-08-21 01:14:36 +0900

    [Util] Hex String을 Byte Array로 변환 유틸 함수 추가

--- Util/PrettyConverter.cs
+++ Util/PrettyConverter.cs
@@ -8,5 +8,24 @@
             string[] hex = Array.ConvertAll(data, x => x.ToString("X2"));
             return string.Join(" ", hex);
         }
+
+        public static byte[]? HexToByteArray(string hexString)
+        {
+            string pureHexString = hexString.Replace(" ", "");
+
+            if (pureHexString.Length == 0 || pureHexString.Length % 2 == 1)
+            {
+                return null;
+            }
+
+            byte[] result = new byte[pureHexString.Length / 2];
+
+            for (int i = 0; i < pureHexString.Length; i += 2)
+            {
+                result[i / 2] = Convert.ToByte(pureHexString.Substring(i, 2), 16);
+            }
+
+            return result;
+        }
     }
 }
